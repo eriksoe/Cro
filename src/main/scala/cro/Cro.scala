f0372245606ce661f4316d09b688e6ecf2717c90
Erik Søe Sørensen <erik+gh@nogetmed.dk>
2010-05-14 02:37:13 +0200

    Added copyright notices.

--- src/main/scala/cro/Cro.scala
+++ src/main/scala/cro/Cro.scala
@@ -1,3 +1,21 @@
+/**
+ * This file is part of Cro - Closure reopening in Java
+ *
+ * Copyright (c) 2010 by Erik Søe Sørensen
+ *
+ * Licensed under the GNU Lesser General Public License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/lgpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 package cro;
 
 import java.lang.reflect.{Field,Modifier,AccessibleObject}
--- src/test/scala/cro/TokenizerBenchmark.scala
+++ src/test/scala/cro/TokenizerBenchmark.scala
@@ -1,3 +1,21 @@
+/**
+ * This file is part of Cro - Closure reopening in Java
+ *
+ * Copyright (c) 2010 by Erik Søe Sørensen
+ *
+ * Licensed under the GNU Lesser General Public License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/lgpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 package cro.test;
 
 import scala.util.parsing.combinator._
@@ -11,8 +29,6 @@ object TokenizerBenchmark extends Lexer {
   // Configure lexical parsing
   reserved ++= List("true", "false", "null")
   delimiters ++= List("{", "}", "[", "]", ":", ",")
-
-  def myToken1 = rep1(letter) | token
 
   override def delim = super.delim
 
